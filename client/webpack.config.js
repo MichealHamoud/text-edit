68e7a0e6e370aabc2a1f0dfc8c1f0f500d98b2c3
MichealHamoud <micheal.hamoud11@outlook.com>
2023-07-06 18:13:09 +1000

    fixed a bug

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,19 +21,16 @@ module.exports = () => {
       path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: "./index.html",
         title: "Just Another Text Editor",
       }),
 
-      // Injects our custom service worker
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
 
-      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
@@ -63,7 +60,6 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
           use: {
             loader: "babel-loader",
             options: {
